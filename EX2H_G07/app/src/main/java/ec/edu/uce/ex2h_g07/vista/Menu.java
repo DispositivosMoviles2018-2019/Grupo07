3808e732f2d8a6900253a1e8e64047cc04d25da5
dscumbajin <dscumbajin@uce.edu.ec>
2019-01-22 16:14:48 -0500

    Examen G07

---
+++ EX2H_G07/app/src/main/java/ec/edu/uce/ex2h_g07/vista/Menu.java
@@ -0,0 +1,33 @@
+package ec.edu.uce.ex2h_g07.vista;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import ec.edu.uce.ex2h_g07.R;
+import ec.edu.uce.ex2h_g07.controlador.VehiculoAdapter;
+
+public class Menu extends AppCompatActivity {
+
+    VehiculoAdapter adaptador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void vehiculos(View view) {
+        Intent intent = new Intent(getApplicationContext(), ListaActivity.class);
+        startActivity(intent);
+        //finish();
+    }
+
+    public void reservas(View view) {
+        Intent intent = new Intent(getApplicationContext(), ReservarActivity.class);
+        startActivity(intent);
+        //finish();
+    }
+}
